07254c41b4ddebe9f276a454b62ddeb10cee40e7
ilyes4CODE <bendaikha.ilyes@univ-ouargla.dz>
2025-10-04 09:37:04 +0100

    port

--- ECH_Backend/settings.py
+++ ECH_Backend/settings.py
@@ -135,7 +135,7 @@ DATABASES = {
         'NAME': 'echsah07_django',
         'USER': 'echsah07_ilyes',
         'PASSWORD': 'B-AV)qVE_$Ko*nuy',
-        'HOST': 'localhost',
+        'HOST': '157.90.66.113',
         'PORT': '3306',  # default MySQL port
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
